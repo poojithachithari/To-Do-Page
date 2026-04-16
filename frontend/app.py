7e18cad3d242a30bbb71f18f236ccff7f1cfbcef
Poojithachithari <poojithachithari@gmail.com>
2026-04-16 11:05:45 +0000

    Changes from frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import requests
+BACKEND_URL = 'http://0.0.0.0:9000'
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    todo_data = dict(request.form)
+    requests.post(BACKEND_URL + '/submit',json=todo_data)
+    return "todo_data submitted successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
